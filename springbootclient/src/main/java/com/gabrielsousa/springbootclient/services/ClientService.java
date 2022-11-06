82d7cb8c515e8f75c398d6d19740c13a16232ffa
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-11-06 19:04:17 -0300

    Client by id, exception handling

--- springbootclient/src/main/java/com/gabrielsousa/springbootclient/services/ClientService.java
+++ springbootclient/src/main/java/com/gabrielsousa/springbootclient/services/ClientService.java
@@ -3,12 +3,14 @@ package com.gabrielsousa.springbootclient.services;
 import com.gabrielsousa.springbootclient.dto.ClientDTO;
 import com.gabrielsousa.springbootclient.entities.Client;
 import com.gabrielsousa.springbootclient.repositories.ClientRepository;
+import com.gabrielsousa.springbootclient.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -20,5 +22,11 @@ public class ClientService {
     public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
         Page<Client> list = repository.findAll(pageRequest);
         return list.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> entity = repository.findById(id);
+        return new ClientDTO(entity.orElseThrow(() -> new ResourceNotFoundException("Id " + id + " not found")));
     }
 }
